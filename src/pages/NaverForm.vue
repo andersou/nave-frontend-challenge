404dbafe4b5b6485664ea0640a9a202d1f4f35e3
Anderson Souza <andersom.eugenio@gmail.com>
2020-11-18 11:09:14 -0300

    adicionado dialogo de sucesso e  modificação para usar o messmo form p edição

--- src/pages/NaverForm.vue
+++ src/pages/NaverForm.vue
@@ -1,13 +1,15 @@
 <template>
   <q-page class="q-px-lg row justify-center">
-    <div class="q-mt-xl" style="width: 800px; max-width:80vw">
+    <div class="q-mt-xl" style="width: 700px; max-width:80vw">
       <div class="row items-center">
         <q-btn
           size="1.5rem"
+          padding="0"
           flat
           round
           color="primary"
           icon="keyboard_arrow_left"
+          to="/"
         />
         <span class="q-pl-md text-h5">Adicionar Naver</span>
       </div>
@@ -46,15 +48,46 @@
           <q-input placeholder="URL da foto do Nave" outlined square dense />
         </div>
       </div>
-      <div class="row items-right">
-        <q-btn></q-btn>
+      <div class="row justify-end">
+        <q-btn
+          color="primary"
+          padding="xs 4rem"
+          class="no-border-radius q-my-sm"
+          dense
+          label="Salvar"
+          @click="dialogSaveSuccess = true"
+          no-caps
+        />
       </div>
     </div>
+    <q-dialog v-model="dialogSaveSuccess" square>
+      <q-card style="width: 600px; max-width: 80vw;">
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">
+            Naver {{ updating ? "atualizado" : "criado" }}
+          </div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section class="q-pb-lg">
+          Naver {{ updating ? "atualizado" : "criado" }} com sucesso!
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "NaverForm",
+  data() {
+    return {
+      dialogSaveSuccess: false,
+      updating: false
+    };
+  }
+};
 </script>
 
 <style></style>
