bd4c5975933c69edbb9ec1362aedc70e76715055
Anderson Souza <andersom.eugenio@gmail.com>
2020-11-18 11:10:19 -0300

    separado dialogo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar class="text-primary bg-white q-pt-md">
         <img style="filter: invert(1)" src="../assets/logo-white.png" />
         <q-space />
-        <q-btn flat label="Sair" />
+        <q-btn @click="logout" flat label="Sair" />
       </q-toolbar>
     </q-header>
 
@@ -15,61 +15,20 @@
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
-const linksData = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev"
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework"
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev"
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev"
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev"
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev"
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev"
-  }
-];
-
+import LoginHelper from "../helpers/login";
 export default {
   name: "MainLayout",
-  components: { EssentialLink },
   data() {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
-    };
+    return {};
+  },
+  methods: {
+    logout() {
+      console.log("logout");
+      let logHelper = new LoginHelper();
+      logHelper.logout();
+      window.lh = logHelper;
+      this.$router.go("/login");
+    }
   }
 };
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,20 +10,16 @@
         dense
         label="Adicionar Naver"
         no-caps
+        to="/create"
       />
     </div>
 
     <q-scroll-area horizontal style="height: calc(100vh - 140px); width: 100%;">
       <div class="row no-wrap q-gutter-md">
-        <div
-          v-for="n in 4"
-          :key="n"
-          style="width: calc((100vw - 80px) / 4)"
-          class="q-pt-md "
-        >
+        <div v-for="n in 4" :key="n" class="q-pt-md naver-card">
           <q-img
             src="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2019/06/25163706/Applications-Software-Developers-1-500x333.jpg"
-            spinner-color="white"
+            spinner-color="black"
             style="filter: grayscale(1);"
             :ratio="1"
             @click="dialogNaverView = true"
@@ -44,61 +40,15 @@
         </div>
       </div>
     </q-scroll-area>
-    <q-dialog square v-model="dialogNaverView">
-      <q-card style="width: 800px; max-width: 80vw;">
-        <q-card-section horizontal>
-          <q-img
-            src="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2019/06/25163706/Applications-Software-Developers-1-500x333.jpg"
-            spinner-color="white"
-            style="filter: grayscale(1);"
-            :ratio="1"
-            @click="dialogNaverView = true"
-            class="col-6"
-          />
-
-          <q-card-section class="full-width ">
-            <div class="row full-width">
-              <div class="col">
-                <p class="text-h6 q-mb-sm">John Doe</p>
-                <p>Software Developer</p>
-                <p class="text-subtitle2 q-mb-sm">
-                  Idade
-                </p>
-                <p class="text-body2">
-                  24 anos
-                </p>
-                <p class="text-subtitle2 q-mb-sm">
-                  Tempo de empresa
-                </p>
-                <p class="text-body2">
-                  24 anos
-                </p>
-                <p class="text-subtitle2 q-mb-sm">
-                  Projetos que participou
-                </p>
-                <p class="text-body2">
-                  24 anos
-                </p>
-              </div>
-            </div>
-            <div class="absolute-bottom q-pa-md">
-              <q-btn
-                @click="dialogConfirm = true"
-                padding="5px"
-                flat
-                round
-                color="primary"
-                icon="delete"
-              />
-              <q-btn padding="5px" flat round color="primary" icon="edit" />
-            </div>
-          </q-card-section>
-          <div class="float-right q-pa-sm">
-            <q-btn icon="close" flat round dense v-close-popup />
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-dialog>
+    <dialog-naver-view
+      :show="dialogNaverView"
+      :naver="naverSelecionado"
+      @close="dialogNaverView = false"
+      @delete="
+        dialogNaverView = false;
+        dialogDeleteSuccess = true;
+      "
+    />
     <q-dialog v-model="dialogDeleteSuccess" square>
       <q-card style="width: 600px; max-width: 80vw;">
         <q-card-section class="row items-center q-pb-none">
@@ -145,16 +95,24 @@
     </q-dialog>
   </q-page>
 </template>
-<style lang="stylus"></style>
+<style lang="stylus">
+.naver-card
+  width: calc((100vw - 80px) / 4)
+</style>
 <script>
+import dialogNaverView from "../components/DialogNaverView";
 export default {
   name: "PageIndex",
   data() {
     return {
       dialogConfirm: false,
       dialogDeleteSuccess: false,
-      dialogNaverView: true
+      dialogNaverView: false,
+      naverSelecionado: ""
     };
+  },
+  components: {
+    dialogNaverView
   }
 };
 </script>
