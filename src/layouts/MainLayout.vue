e0f06a30f42c09b06547c90a3578619e459ec0aa
Anderson Souza <andersom.eugenio@gmail.com>
2020-11-18 14:09:47 -0300

    router link no logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,9 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar class="text-primary bg-white q-pt-md">
-        <img style="filter: invert(1)" src="../assets/logo-white.png" />
+        <router-link to="/" style="display: inline-flex">
+          <img style="filter: invert(1)" src="../assets/logo-white.png" />
+        </router-link>
         <q-space />
         <q-btn @click="logout" flat label="Sair" />
       </q-toolbar>
@@ -21,6 +23,7 @@ export default {
   data() {
     return {};
   },
+  mounted() {},
   methods: {
     logout() {
       console.log("logout");
