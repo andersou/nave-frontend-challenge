6ddc58a958a29dd2014070e66607ea8508297bbe
Anderson Souza <andersom.eugenio@gmail.com>
2020-11-18 08:56:36 -0300

    terminado index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -18,7 +18,7 @@
         <div
           v-for="n in 4"
           :key="n"
-          style="width: calc((100vw - 80px) / 4)" 
+          style="width: calc((100vw - 80px) / 4)"
           class="q-pt-md "
         >
           <q-img
@@ -26,6 +26,7 @@
             spinner-color="white"
             style="filter: grayscale(1);"
             :ratio="1"
+            @click="dialogNaverView = true"
           />
           <br />
           <p class="q-mt-sm q-mb-none text-weight-bold">John Doe</p>
@@ -43,19 +44,59 @@
         </div>
       </div>
     </q-scroll-area>
-    <q-dialog v-model="dialogNaverView">
-      <q-card style="width: 700px; max-width: 80vw;">
-        <q-card-section>
-          <div class="text-h6">Medium</div>
-        </q-card-section>
+    <q-dialog square v-model="dialogNaverView">
+      <q-card style="width: 800px; max-width: 80vw;">
+        <q-card-section horizontal>
+          <q-img
+            src="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2019/06/25163706/Applications-Software-Developers-1-500x333.jpg"
+            spinner-color="white"
+            style="filter: grayscale(1);"
+            :ratio="1"
+            @click="dialogNaverView = true"
+            class="col-6"
+          />
 
-        <q-card-section class="q-pt-none">
-          Click/Tap on the backdrop.
+          <q-card-section class="full-width ">
+            <div class="row full-width">
+              <div class="col">
+                <p class="text-h6 q-mb-sm">John Doe</p>
+                <p>Software Developer</p>
+                <p class="text-subtitle2 q-mb-sm">
+                  Idade
+                </p>
+                <p class="text-body2">
+                  24 anos
+                </p>
+                <p class="text-subtitle2 q-mb-sm">
+                  Tempo de empresa
+                </p>
+                <p class="text-body2">
+                  24 anos
+                </p>
+                <p class="text-subtitle2 q-mb-sm">
+                  Projetos que participou
+                </p>
+                <p class="text-body2">
+                  24 anos
+                </p>
+              </div>
+            </div>
+            <div class="absolute-bottom q-pa-md">
+              <q-btn
+                @click="dialogConfirm = true"
+                padding="5px"
+                flat
+                round
+                color="primary"
+                icon="delete"
+              />
+              <q-btn padding="5px" flat round color="primary" icon="edit" />
+            </div>
+          </q-card-section>
+          <div class="float-right q-pa-sm">
+            <q-btn icon="close" flat round dense v-close-popup />
+          </div>
         </q-card-section>
-
-        <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn flat label="OK" v-close-popup />
-        </q-card-actions>
       </q-card>
     </q-dialog>
     <q-dialog v-model="dialogDeleteSuccess" square>
@@ -112,7 +153,7 @@ export default {
     return {
       dialogConfirm: false,
       dialogDeleteSuccess: false,
-      dialogNaverView: false
+      dialogNaverView: true
     };
   }
 };
